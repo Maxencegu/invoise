562f4c419b37210eb3ac5a1f8eb14604d7af3ab1
mguery <mguery@effisoft-group.com>
2022-09-09 22:31:59 +0200

    Spring MVC 1er controller

--- core/src/main/java/com/mycompany/invoise/controller/web/InvoiceControllerWeb.java
+++ core/src/main/java/com/mycompany/invoise/controller/web/InvoiceControllerWeb.java
@@ -5,6 +5,7 @@ import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.service.InvoiceServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InvoiceControllerWeb implements InvoiceControllerInterface {
@@ -22,5 +23,11 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     @Override
     public void setInvoiceService(InvoiceServiceInterface invoiceService) {
         this.invoiceService = invoiceService;
+    }
+
+    @RequestMapping("/invoice-home")
+    public String displayHome() {
+        System.out.println("La méthode displayHome a bien été invoquée.");
+        return "";
     }
 }

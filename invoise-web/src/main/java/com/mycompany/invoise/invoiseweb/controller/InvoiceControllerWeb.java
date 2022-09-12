deca68b54c927ff0739349dc87503fce728f2e07
Maxencegu <maxenceguery@yahoo.fr>
2022-09-12 22:44:53 +0200

    Add files via upload

---
+++ invoise-web/src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -0,0 +1,37 @@
+package com.mycompany.invoise.invoiseweb.controller;
+
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.core.entity.Invoice;
+import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class InvoiceControllerWeb implements InvoiceControllerInterface {
+    @Autowired
+    private InvoiceServiceInterface invoiceService;
+
+    @Override
+    public void createInvoice() {
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName("Tesla");
+
+        invoiceService.createInvoice(invoice);
+    }
+
+    @Override
+    public void setInvoiceService(InvoiceServiceInterface invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @RequestMapping("/invoice-home")
+    public @ModelAttribute("invoices") List<Invoice> displayHome() {
+        System.out.println("La méthode displayHome a bien été invoquée.");
+        List<Invoice> invoices = invoiceService.getInvoices();
+        return invoices;
+    }
+}
